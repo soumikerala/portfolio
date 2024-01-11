d0cfd3f6a8f69a4a5b7baea0ee680ff3660dde82
soumiyamenon <soumikerala1999@gmail.com>
2024-01-11 12:41:21 +0530

    my portfolio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 import './home.css';
 import Image from '../Assets/soumi.jpg';
-import{useNavigate} from "react-router-dom"
+
 
 function Home(){
-  const navigate= useNavigate();
+  
   return(
     <>
     <div className="home">
       <div className="Text">
       <h1>Hi,SoumiyaMenon</h1>
       <h2> FrontEnd Developer</h2>
-      <button onClick={()=> navigate('/about')}> go to about page</button>
+      
      
       </div>
       
